916b62e94805b0bb49cbfe0764a656ccf391218b
Hiro-yoshi <161566433+Hiro-yoshi@users.noreply.github.com>
2024-06-12 10:28:42 +0900

    ユーザー管理機能の実装_修正3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -128,6 +128,12 @@ RSpec.describe User, type: :model do
         @user.valid?
         expect(@user.errors.full_messages).to include("Mei kana is invalid. Input full-width katakana characters.")
       end
+
+      it "生年月日が空だと登録できないこと" do
+        @user.birth_date = ""
+        @user.valid?
+        expect(@user.errors.full_messages).to include("Birth date can't be blank")
+      end
     end
   end
 end
