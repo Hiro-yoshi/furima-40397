f3779915e7b7cdd62e65738edcce59703f4cd890
Hiro-yoshi <161566433+Hiro-yoshi@users.noreply.github.com>
2024-06-22 14:04:16 +0900

    商品状態をデータベースに保存

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,25 @@
 class ItemsController < ApplicationController
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @user = current_user
+    if @item.save
+      redirect_to root_path, notice: "商品が出品されました"
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :delivery_time_id, :price).merge(user_id: current_user.id)
   end
 end
