6a515633c4303e76cae2b9ad5387b1b434a2b9f1
Hiro-yoshi <161566433+Hiro-yoshi@users.noreply.github.com>
2024-07-02 14:50:19 +0900

    商品情報編集機能の実装_修正1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+  before_action :set_item, only: [:show, :edit, :update]
   before_action :check_item_owner, only: [:edit, :update]
 
   def index
@@ -21,15 +22,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])  
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to root_path
     else
@@ -43,8 +41,11 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id, :delivery_time_id, :price).merge(user_id: current_user.id)
   end
 
-  def check_item_owner
+  def set_item
     @item = Item.find(params[:id])
+  end
+
+  def check_item_owner
     unless @item.user == current_user
       redirect_to root_path
     end
