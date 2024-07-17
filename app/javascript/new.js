db2279ee5552448618f1ddfec5fac0895b864c75
Hiro-yoshi <161566433+Hiro-yoshi@users.noreply.github.com>
2024-07-17 18:30:28 +0900

    再出品時のnew.jsの修正

--- app/javascript/new.js
+++ app/javascript/new.js
@@ -1,14 +1,24 @@
-document.addEventListener('turbo:load', () => {
+const setupPriceCalculation = () => {
   const priceInput = document.getElementById('item-price');
   const taxPriceDom = document.getElementById('add-tax-price');
   const profitDom = document.getElementById('profit');
 
-  priceInput.addEventListener('input', () => {
+  const calculatePrice = () => {
     const inputValue = priceInput.value;
-    const tax = Math.floor(inputValue * 0.1); // 販売手数料は10%
-    const profit = Math.floor(inputValue - tax); // 販売利益は価格から手数料を引いた額
+    const tax = Math.floor(inputValue * 0.1);
+    const profit = Math.floor(inputValue - tax);
 
     taxPriceDom.innerHTML = tax;
     profitDom.innerHTML = profit;
-  });
-});
+  };
+
+  if (priceInput) {
+    if (priceInput.value) {
+      calculatePrice();
+    }
+    priceInput.addEventListener('input', calculatePrice);
+  }
+};
+
+document.addEventListener("turbo:load", setupPriceCalculation);
+document.addEventListener("turbo:render", setupPriceCalculation);
